5a1d849565e3aeb4fb15b67569c9f56d6ed971f5
Sander Smits <sander.smits@nelen-schuurmans.nl>
2013-03-07 17:04:41 +0100

    Add HG_CMD to settings.

--- model_databank/testsettings.py
+++ model_databank/testsettings.py
@@ -82,6 +82,7 @@ STATIC_ROOT = os.path.join(BUILDOUT_DIR, 'var', 'static')
 MEDIA_ROOT = os.path.join(BUILDOUT_DIR, 'var', 'media')
 #STATICFILES_FINDERS = STATICFILES_FINDERS
 
+HG_CMD = '/usr/local/bin/hg'
 
 try:
     # Import local settings that aren't stored in svn/git.
