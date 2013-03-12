8b00a9babab994f65b8f1c7e26075e157be416ba
Sander Smits <sander.smits@nelen-schuurmans.nl>
2013-03-12 14:24:41 +0100

    Use better names for variables (not builtins).

--- model_databank/utils.py
+++ model_databank/utils.py
@@ -6,14 +6,14 @@ from model_databank.conf import settings
 from model_databank.vcs_utils import get_latest_revision
 
 
-def zip_dir(path, zip):
+def zip_dir(path, zip_file):
     for root, dirs, files in os.walk(path):
         if '.hg' in root:
             continue
-        for file in files:
-            filename = os.path.join(root, file)
-            arcname = os.path.join(root.lstrip(path), file)
-            zip.write(filename, arcname, zipfile.ZIP_DEFLATED)
+        for _file in files:
+            filename = os.path.join(root, _file)
+            arcname = os.path.join(root.lstrip(path), _file)
+            zip_file.write(filename, arcname, zipfile.ZIP_DEFLATED)
 
 
 def zip_model_files(model_reference):
@@ -24,7 +24,7 @@ def zip_model_files(model_reference):
     file_name = '%s-%s.zip' % (model_reference.uuid, latest_revision)
     zip_file_full_path = os.path.join(settings.MODEL_DATABANK_DOWNLOAD_PATH,
                                       file_name)
-    zip = zipfile.ZipFile(zip_file_full_path, 'w')
-    zip_dir(repo_path, zip)
-    zip.close()
+    zip_file = zipfile.ZipFile(zip_file_full_path, 'w')
+    zip_dir(repo_path, zip_file)
+    zip_file.close()
     return zip_file_full_path, latest_revision
